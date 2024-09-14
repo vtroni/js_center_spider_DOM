30f60382fd1ae98f7b8af8571a757a8ec2aeeb45
Virineya Marchuk <marchukvirineya@gmail.com>
2024-09-14 16:24:47 +0300

    make build

---
+++ index.49f75aee.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider"),l=e.clientWidth,i=e.clientHeight,c=t.clientWidth,n=t.clientHeight;t.style.top=i/2-n/2+"px",t.style.left=l/2-c/2+"px";
+//# sourceMappingURL=index.49f75aee.js.map
